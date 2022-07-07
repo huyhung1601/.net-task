18bddc783e656220824b6ce532299dada00c29e3
Henry Nguyen <huyhung1601@gmail.com>
2022-07-07 12:32:38 +1000

    task 6

--- ProgrammingExercises-netcore/Exercise6/Program.cs
+++ ProgrammingExercises-netcore/Exercise6/Program.cs
@@ -10,8 +10,8 @@ namespace CISS_ProgrammingExercise
     {
         public class Line
         {
-            public Point P1 { get; set; }
-            public Point P2 { get; set; }
+            public System.Drawing.Point P1 { get; set; }
+            public System.Drawing.Point P2 { get; set; }
         }
 
         public static double ToDegrees(double radians)
@@ -64,7 +64,36 @@ namespace CISS_ProgrammingExercise
         // the line class has 2 properties P1 & P2, that are used as drawing Points (X,Y)
         public IEnumerable<Line> Exercise6(Rectangle rect)
         {
-            return null;
+            var width = rect.Width;
+            var height = rect.Height;
+            var radius = width / 2;
+            var centerPoint = new Point(rect.X + (width / 2), rect.Y + (height / 2));
+            List<Line> lines = new List<Line>();
+            for (double i = 0; i < 360; i++)
+            {
+
+                var xChange = (int)(radius * Math.Cos(i));
+                var yChange = (int)(radius * Math.Sin(i));
+
+                var point2 = new Point(rect.X + rect.Width / 2, centerPoint.Y);
+                point2.X -= xChange;
+                if (i < 180)
+                {
+                    point2.Y = (int)(point2.Y + yChange);
+                }
+                else
+                {
+                    point2.Y = (int)(point2.Y - yChange);
+                };
+                Console.WriteLine(point2);
+                Line line = new Line();
+                line.P1 = centerPoint;
+                line.P2 = point2;
+                lines.Add(line);
+            }
+            Console.WriteLine(lines.ToString());
+
+            return lines;
         }
     }
 }
